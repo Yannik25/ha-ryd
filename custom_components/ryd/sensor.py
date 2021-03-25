6af9174565f18a9082f75f7b59a803c5b10bda8f
Yannik25 <yannik92@me.com>
2021-03-25 17:38:15 +0100

    update sensor.py to work with PyPi

--- custom_components/ryd/sensor.py
+++ custom_components/ryd/sensor.py
@@ -11,13 +11,17 @@ from homeassistant.const import CONF_URL, CONF_EMAIL, CONF_PASSWORD
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
+
 _LOGGER = logging.getLogger(__name__)
+
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_URL): cv.url,
         vol.Required(CONF_EMAIL): cv.matches_regex(r".+@.+\..+"),
         vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL):cv.int,
     }
 )
 
@@ -25,95 +29,34 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 def setup_platform(hass: core.HomeAssistant, config: dict, add_devices, discovery_info=None) -> bool:
     """Set up the RYD Custom Component."""
     # @TODO: Add setup code.
+    from pyryd import Ryd
     url = config.get(CONF_URL)
     email = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
-    add_devices([RydSensor(url, email, password)])
+    scan_interval = config.get(CONF_SCAN_INTERVAL)
+    
+    
+    ryd = Ryd(url,email,password)
+    # fetch data once at set-up
+    ryd.fetch()
+    fetch = lambda: ryd.fetch()
+    track_time_interval(hass, fetch, scan_interval)
+    
+    add_devices([RydAdapter(ryd)])
     return True
 
 
-class RydSensor(Entity):
+class RydAdapter(Entity):
     """Representation of a ryd sensor."""
-
-    def __init__(self, url: str, email: str, password: str):
-        # copied from https://github.com/NemoN/ioBroker.ryd/blob/master/io-package.json
-        self._ryd_api_server = url
-        self._ryd_app_version = "2.52.4(201008000)"
-        self._client_device_version = "9.0.0"
-        self._client_device_resolution = "2960x1440"
-        self._client_device_id = "SM-G960F"
-        self._client_device_type = "Android"
-        self._think_properties = "curLocation,parkingLocation,carOdometer,estimates,reportedFuelTotal,fuel"
-        self._think_properties_ignore = "recurrences,openDtcs,score"
-        self._ryd_app_locale = "de-de"
-        self._ryd_app_internal_name = "TankTaler"
-        self._ryd_app_platform = "{} [{},{},{}]".format(
-            self._client_device_type, self._client_device_id, self._client_device_version, self._client_device_resolution
-        )
-        self._ryd_app_user_agent = "{}/{}({};{} {})".format(
-            self._ryd_app_internal_name, self._ryd_app_version, self._client_device_id, self._client_device_type, self._client_device_version
-        )
-        self._data = json.dumps({"email": email, "password": password})
-        self._headers = {
-            'x-txn-platform': self._ryd_app_platform,
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            'Pragma': 'no-cache',
-            'Expires': str(0),
-            'x-txn-app-version': self._ryd_app_version,
-            'User-agent': self._ryd_app_user_agent,
-            'X-Txn-Request-Id': str(uuid.uuid4()),
-            'X-Txn-Locale': self._ryd_app_locale,
-            'Content-Type': 'application/json; charset=utf-8',
-        }
-        self._attributes = {}
-        self._state = None
-        self._unit_of_measurement = None
-        self._icon = None
-
-    @property
-    def state(self):
-        """Return the state."""
-        return self._state
-
-    @property
-    def device_state_attributes(self) -> Optional[Dict[str, Any]]:
-        """Return the unit of measurement."""
-        return self._attributes
-    @property
-    def unit_of_measurement(self) -> Optional[str]:
-        return "l"  # litre
-
+        
+    def __init__(self, ryd: Ryd):
+        self.ryd = ryd    
+    
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-
-        response = requests.post(
-            self._ryd_api_server +
-            "/auth%2Flogin%2Flocal",
-            data=self._data,
-            headers=self._headers,
-            timeout=2000
-        )
-        json_object = json.loads(response.text)
-
-        ryd_auth_token=json_object["auth_token"]
-        rydid = json_object["things"][0]["id"]
-
-        response = requests.get(
-            self._ryd_api_server +
-            "/things/" +
-            rydid +
-            "/status?auth_token=" +
-            ryd_auth_token,
-            headers=self._headers,
-            timeout=2000
-        )
-        json_data = response.json()
         
-        self._attributes = json_data["data"]
-        attributes = self._attributes
-
         # Add discovered value fields as sensors
         # because some fields are only sent temporarily
         new_sensors = []
