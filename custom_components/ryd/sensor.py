5b59f10a32cdcbdc087864e2ae5473a33fe6ccfc
Niels Mündler <n.muendler@tum.de>
2021-03-25 18:10:18 +0100

    Final changes to ryd sensor

--- custom_components/ryd/sensor.py
+++ custom_components/ryd/sensor.py
@@ -1,27 +1,25 @@
 """Support for RYD sensors."""
-from typing import Optional, Dict, Any
+from typing import Set
 import logging
-import requests
-import json
-import uuid
 import voluptuous as vol
+from datetime import timedelta
 from homeassistant import core
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_URL, CONF_EMAIL, CONF_PASSWORD
+from homeassistant.const import CONF_URL, CONF_EMAIL, CONF_PASSWORD, CONF_SCAN_INTERVAL
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import track_time_interval
 
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=5)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_URL): cv.url,
         vol.Required(CONF_EMAIL): cv.matches_regex(r".+@.+\..+"),
         vol.Required(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_SCAN_INTERVAL):cv.int,
     }
 )
 
@@ -33,39 +31,48 @@ def setup_platform(hass: core.HomeAssistant, config: dict, add_devices, discover
     url = config.get(CONF_URL)
     email = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
-    scan_interval = config.get(CONF_SCAN_INTERVAL)
-    
-    
+    scan_interval = config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+
     ryd = Ryd(url,email,password)
-    # fetch data once at set-up
-    ryd.fetch()
-    fetch = lambda: ryd.fetch()
+    ryd_adapter = RydAdapter(ryd, add_devices)
+    fetch = lambda: ryd_adapter.update()
     track_time_interval(hass, fetch, scan_interval)
     
-    add_devices([RydAdapter(ryd)])
     return True
 
 
 class RydAdapter(Entity):
     """Representation of a ryd sensor."""
         
-    def __init__(self, ryd: Ryd):
-        self.ryd = ryd    
+    def __init__(self, ryd, add_devices):
+        self.ryd = ryd
+        self._add_devices = add_devices
+        self.values = {}
+        self._sensors = set()
+        self._registered_sensors : Set[RydTemplateSensor] = set()
     
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        
+        try:
+            self.ryd.fetch()
+        except ConnectionError:
+            _LOGGER.error(
+                "Failed to update: No Connection."
+            )
+            return
+
+        self.values = self.ryd._ref_data
         # Add discovered value fields as sensors
         # because some fields are only sent temporarily
         new_sensors = []
-        for key in attributes:
-            if key not in self.sensors:
-                self.sensors.add(key)
+        for key in self.values:
+            if key not in self._sensors:
+                self._sensors.add(key)
                 _LOGGER.info("Discovered %s, adding as sensor", key)
                 new_sensors.append(RydTemplateSensor(self, key))
-        self._add_entities(new_sensors, True)
+        self._add_devices(new_sensors, True)
 
         # Schedule an update for all included sensors
         for sensor in self._registered_sensors:
@@ -75,10 +82,11 @@ class RydAdapter(Entity):
         """Register child sensor for update subscriptions."""
         self._registered_sensors.add(sensor)
 
+
 class RydTemplateSensor(Entity):
     """Sensor for the single values (e.g. Fuel, Location...)."""
 
-    def __init__(self, parent: RydSensor, name):
+    def __init__(self, parent: RydAdapter, name):
         """Initialize a singular value sensor."""
         self._name = name
         self.parent = parent
@@ -112,7 +120,7 @@ class RydTemplateSensor(Entity):
 
     async def async_update(self):
         """Update the internal state."""
-        state = self.parent.data.get(self._name)
+        state = self.parent.values.get(self._name)
         self._state = state.get("value")
         self._unit = state.get("unit")
 
